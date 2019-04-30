e79ce2d66e983a429939e95dff49db5a8a27f2bc
Alex91russ <k_alex91@mail.ru>
2019-04-30 23:40:18 +0400

    step 2

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
+const actualName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${actualName}!`);
