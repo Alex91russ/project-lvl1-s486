b47d6fdc0293c7606afdc03e364f5e70acea2a88
Alex91russ <k_alex91@mail.ru>
2019-05-03 19:14:44 +0400

    refactoring

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -1,23 +1,14 @@
-import readlineSync from 'readline-sync';
+import startGame from '..';
+import { getNumber } from '../functions';
 
-const hasEven = () => {
-  const getName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${getName}!`);
-  let count = 0;
-  for (let i = 0; i < 3; i += 1) {
-    const getNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-    const number = getNumber(1, 49);
-    console.log(`Question: ${number}`);
-    const correntAnswer = readlineSync.question('Your answer: ');
-    if ((number % 2 === 0 && correntAnswer === 'yes') || (number % 2 === 1 && correntAnswer === 'no')) {
-      console.log('Corrent!');
-      count += 1;
-    } else return console.log(`"yes" is wrong answer ;(. Correct answer was "no". Let's try again, ${getName}`);
-  }
-  if (count === 3) {
-    console.log(`Congratulations, ${getName}!`);
-  }
-  return console.log('');
+const instruction = 'Answer "yes" if number even otherwise answer "no". \n';
+
+const brainEven = () => {
+  const number = getNumber(1, 49);
+  return {
+    question: `${number}`,
+    answer: number % 2 === 0 ? 'yes' : 'no',
+  };
 };
 
-export default hasEven;
+export default () => startGame(brainEven, instruction);
