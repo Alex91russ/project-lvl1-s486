5d4da82b01eba6edb484f6738103278f226b7abe
Alex91russ <k_alex91@mail.ru>
2019-05-09 13:49:59 +0400

    add build progression and refactoring

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,29 +1,30 @@
 import startGame from '..';
 import { getNumber } from '../utils';
 
-const instruction = 'What number is missing in the progression? \n';
+const instruction = 'What number is missing in the progression?';
+
+const buildProgression = (startNumber, step, numberSteps) => {
+  const makeProgression = (el, acc) => {
+    const newProgression = [...acc, el];
+    if (newProgression.length === numberSteps) {
+      return newProgression;
+    }
+    return makeProgression(el + step, newProgression);
+  };
+  return makeProgression(startNumber, []);
+};
 
 const brainProgression = () => {
   const startNumber = getNumber(1, 15);
   const step = getNumber(1, 10);
-  const changeNumber = getNumber(1, 10);
-  const progression = [startNumber];
-  let nextNumber = startNumber + step;
-  const funcProgression = (array) => {
-    while (array.length < 10) {
-      array.push(nextNumber);
-      nextNumber += step;
-    }
-    return array;
-  };
-  const startArray = funcProgression(progression);
-  const changeArray = startArray.slice();
-  changeArray[changeNumber] = '..';
-
-  return {
-    question: `${changeArray}`,
-    answer: String(startArray[changeNumber]),
-  };
+  const numberSteps = 10;
+  const progression = buildProgression(startNumber, step, numberSteps);
+  const hiddenElementPosition = getNumber(0, 10);
+  const newProgression = progression.slice();
+  newProgression[hiddenElementPosition] = '..';
+  const question = `${newProgression}`;
+  const answer = String(progression[hiddenElementPosition]);
+  return { question, answer };
 };
 
 export default () => startGame(brainProgression, instruction);
