bc98ea7a67ad050e6c1ea93f2dc20984bc89cbf0
Alex91russ <k_alex91@mail.ru>
2019-05-09 13:47:57 +0400

    chande isPrime and refactoring

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,25 +1,24 @@
 import startGame from '..';
-import { getNumber } from '../functions';
+import { getNumber } from '../utils';
 
-const instruction = 'Answer "yes" if given number is prime. Otherwise answer "no". \n';
+const instruction = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+  const stop = num / 2;
+  const iter = (counter) => {
+    if (counter > stop) return true;
+    if (num % counter === 0) return false;
+    return iter(counter + 1);
+  };
+  return iter(2);
+};
 
 const brainPrime = () => {
   const number = getNumber(1, 50);
-  const check = (num) => {
-    if (num < 2) return false;
-    const stop = num / 2;
-    const iter = (counter) => {
-      if (counter > stop) return true;
-      if (num % counter === 0) return false;
-      return iter(counter + 1);
-    };
-    return iter(2);
-  };
-
-  return {
-    question: `${number}`,
-    answer: check(number) ? 'yes' : 'no',
-  };
+  const question = `${number}`;
+  const answer = isPrime(number) ? 'yes' : 'no';
+  return { question, answer };
 };
 
 export default () => startGame(brainPrime, instruction);
