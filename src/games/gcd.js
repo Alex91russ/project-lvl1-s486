86950a348ad97515f4d3cfe66af34eb2f3fd500e
Alex91russ <k_alex91@mail.ru>
2019-05-09 13:46:14 +0400

    refactoring

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,22 +1,21 @@
 import startGame from '..';
-import { getNumber } from '../functions';
+import { getNumber } from '../utils';
 
-const instruction = 'Find the greatest common divisor of given numbers. \n';
+const instruction = 'Find the greatest common divisor of given numbers.';
+
+const getRightAsnwer = (num1, num2) => {
+  if (num2 === 0) {
+    return num1;
+  }
+  return getRightAsnwer(num2, num1 % num2);
+};
 
 const brainGcd = () => {
-  const number1 = getNumber(1, 100);
-  const number2 = getNumber(1, 100);
-
-  const getRightAsnwer = (num1, num2) => {
-    if (num2 === 0) {
-      return num1;
-    }
-    return getRightAsnwer(num2, num1 % num2);
-  };
-  return {
-    question: `${number1} ${number2}`,
-    answer: String(getRightAsnwer(number1, number2)),
-  };
+  const firstNumber = getNumber(1, 100);
+  const secondNumber = getNumber(1, 100);
+  const question = `${firstNumber} ${secondNumber}`;
+  const answer = String(getRightAsnwer(firstNumber, secondNumber));
+  return { question, answer };
 };
 
 export default () => startGame(brainGcd, instruction);
