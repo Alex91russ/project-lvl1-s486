846b3b330d1d1dc9669c95a3488727659d1566fc
Alex91russ <k_alex91@mail.ru>
2019-05-09 13:55:57 +0400

    refactoring

--- src/functions.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-
-export const getName = () => {
-  const actualName = readlineSync.question('May I have your name? ');
-  return actualName;
-};
-
-export const getNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
